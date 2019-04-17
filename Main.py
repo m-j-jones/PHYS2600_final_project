345fe89048e1b630041591a629a06f1b6aafa1e9
Matt Jones <>
2019-04-16 20:14:33 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Main script for PHYS 2600 final project.
+
+"""
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from random import random, randrange
+from numba import jit
+from copy import deepcopy #is this necessary?
+
+
+# Set up parameters
+
+
+# Run simulation
+
+
+# Analyze results
+
+
+# Produce plots
+
---
+++ functions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Source file for functions used in Main.py
+
+"""
+
+# Import libraries
+import numpy as np
+from numba import jit
+
+# Function definitions
